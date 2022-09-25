201a0a3d66cd0293a935c14fe5152460cab8c179
Ritik sharma <98308171+Emritik@users.noreply.github.com>
2022-09-25 11:50:34 +0530

    Update Make-call.py

--- Make-call.py
+++ Make-call.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 
 #neter your account_sid and auth_token
 
-account_sid = os.environ['AC087d7d180bf753f0993c3846b729e72e']
-auth_token = os.environ['8587cc122360d7fe71a11d0172a18242']
+account_sid = os.environ['enter here your account sid']
+auth_token = os.environ['enter here your auth token no.']
 client = Client(account_sid, auth_token)
 
 #enter your freind number in "to" section.
