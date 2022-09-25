51e0fd00cef683cea19c7673ed9def482d743a35
Chirag Arora <84070677+Chirag8023@users.noreply.github.com>
2022-09-25 18:49:30 +0530

    Update Make-call.py
    Correct some grammatical & spelling error, 🍕
    Also remove some personal info from code like Personal phone No. & Twilio Number 🍕

--- Make-call.py
+++ Make-call.py
@@ -3,19 +3,19 @@
 import os
 from twilio.rest import Client
 
-#neter your account_sid and auth_token
+#Enter your Twilio Account SID and Auth Token.
 
-account_sid = os.environ['enter here your account sid']
-auth_token = os.environ['enter here your auth token no.']
+account_sid = os.environ['enter here your Account SID : ']
+auth_token = os.environ['enter here your Auth Token : ']
 client = Client(account_sid, auth_token)
 
-#enter your freind number in "to" section.
-#enter a twilio number (which is buy form console twilio) in "from-" section.
+
+
 
 call = client.calls.create(
                         url='http://demo.twilio.com/docs/voice.xml',
-                        to='+19285978342',
-                        from_='+919125545701'
+                        to='XXXXXXXXX',      #Enter your friend's number in "to" section.
+                        from_='+91XXXXXXXXXX'   #Enter your twilio number ( which bought from Twilio console ) in "from_" section.
                         )
 
 print(call.sid)
